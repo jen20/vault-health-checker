8174596509fd6644136d7468a42aa7249ccc64a8
James Nugent <james@jen20.com>
2018-06-07 23:13:27 -0500

    Actually handle connections from NLB health check

--- tcp_listener.go
+++ tcp_listener.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"time"
 
 	log "github.com/hashicorp/go-hclog"
 )
@@ -14,6 +15,7 @@ type tcpListener struct {
 	logger       log.Logger
 
 	listener net.Listener
+	shutdown chan struct{}
 }
 
 func newTCPListener(addr string, standbyOK bool, logger log.Logger, statusChange <-chan vaultStatus) *tcpListener {
@@ -22,6 +24,8 @@ func newTCPListener(addr string, standbyOK bool, logger log.Logger, statusChange
 		standbyOK:    standbyOK,
 		logger:       logger,
 		statusChange: statusChange,
+
+		shutdown: make(chan struct{}, 1),
 	}
 }
 
@@ -53,17 +57,45 @@ func (tl *tcpListener) run() error {
 				continue
 			}
 
-			listener, err := net.Listen("tcp", tl.addr)
-			if err != nil {
-				tl.logger.Error(fmt.Sprintf("TCP Listener Error: %s", err))
-			}
-			tl.listener = listener
-			tl.logger.Info(fmt.Sprintf("Listening on %s...", tl.addr))
+			go tl.runListener()
 		} else {
 			if tl.listener != nil {
+				tl.shutdown <- struct{}{}
 				tl.listener.Close()
 				tl.logger.Info(fmt.Sprintf("Listener %s closed", tl.addr))
 			}
 		}
 	}
+}
+
+func (tl *tcpListener) runListener() {
+	listener, err := net.Listen("tcp", tl.addr)
+	if err != nil {
+		tl.logger.Error(fmt.Sprintf("TCP Listener Error: %s", err))
+		return
+	}
+	tl.logger.Info(fmt.Sprintf("Listening on %s...", tl.addr))
+	tl.listener = listener
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			tl.logger.Error(fmt.Sprintf("Error accepting connection: %s", err))
+
+			select {
+			case <-tl.shutdown:
+				break
+			default:
+				continue
+			}
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	time.Sleep(500 * time.Millisecond)
 }
